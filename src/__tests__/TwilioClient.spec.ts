666054112923f2904c402fca39e034d6a79fa49c
actionchain <actionchain@chainguard.dev>
2026-06-19 02:32:09 +0000

    Update fabasoad/twilio-voice-call-action@v3.0.7

---
+++ src/__tests__/TwilioClient.spec.ts
@@ -0,0 +1,60 @@
+import { vi, describe, test, expect } from 'vitest';
+import { TwilioClient, TwilioVoiceNotSupportedException } from '../TwilioClient';
+import type { ClientOpts } from 'twilio';
+
+const twilioFn = vi.fn();
+const twilioCallsCreateFn = vi.fn();
+
+vi.mock('twilio', () => {
+  class Twilio {
+    // biome-ignore lint/suspicious/noExplicitAny: used for tests
+    calls: { create: (...args: any[]) => any };
+    constructor(username?: string, password?: string, opts?: ClientOpts) {
+      twilioFn(username, password, opts);
+      // biome-ignore lint/suspicious/noExplicitAny: used for tests
+      this.calls = { create: (...createArgs: any[]) => twilioCallsCreateFn(...createArgs) };
+    }
+  }
+  return { Twilio };
+})
+
+describe('Twilio Client', () => {
+  const expectedAccountSid = 'account-sid-test';
+  const expectedAuthToken = 'auth-token-test';
+  const expectedLogLevel = 'log-level-test';
+  const expectedText = 'text-test';
+  const expectedFrom = 'from-test';
+  const expectedTo = 'to-test';
+
+  describe('Positive scenarios', () => {
+    test.each(['man', 'woman', 'alice'])('when %s voice is set then should pass', async (expectedVoice: string) => {
+      // Act
+      const twilioClient = new TwilioClient({ accountSid: expectedAccountSid, authToken: expectedAuthToken, logLevel: expectedLogLevel });
+      await twilioClient.call(expectedVoice, expectedText, expectedFrom, expectedTo);
+      // Assert
+      expect(twilioFn).toHaveBeenCalledWith(
+        expectedAccountSid, expectedAuthToken, { lazyLoading: true, logLevel: expectedLogLevel }
+      );
+      expect(twilioCallsCreateFn).toHaveBeenCalledWith({
+        from: expectedFrom,
+        to: expectedTo,
+        twiml: `<Response><Say voice="${expectedVoice}">${expectedText}</Say></Response>`,
+      });
+    })
+  })
+
+  describe('Negative scenarios', () => {
+    test('when unsupported voice is set then should throw error', async () => {
+      // Act
+      const twilioClient = new TwilioClient({ accountSid: expectedAccountSid, authToken: expectedAuthToken, logLevel: expectedLogLevel });
+      const fnCall = () => twilioClient.call(
+        'unsupported', expectedText, expectedFrom, expectedTo,
+      );
+      // Assert
+      expect(twilioFn).toHaveBeenCalledWith(
+        expectedAccountSid, expectedAuthToken, { lazyLoading: true, logLevel: expectedLogLevel },
+      );
+      await expect(fnCall).rejects.toThrow(TwilioVoiceNotSupportedException);
+    })
+  })
+})
